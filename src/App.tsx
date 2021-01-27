0650fbfb6f8b396179e8c1e30f5b50fbd7d969b2
A Faruk Gonullu <afgonullu@gmail.com>
2021-01-28 01:43:11 +0300

    build: implement AuthProvider, app-wide auth State

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { LogIn } from "react-feather";
 
 import Header from "./layouts/Header";
 import Home from "./pages/Home";
 
 import "./App.scss";
+import { AuthProvider } from "./context/auth";
+import Login from "./pages/Login";
 
 const App: React.FC = () => {
   return (
-    <Router>
-      <Header />
-      <Switch>
-        <Route path="/login">
-          <h2>
-            <LogIn />
-            Login
-          </h2>
-        </Route>
-        <Route path="/register">
-          <h2>Register</h2>
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <AuthProvider>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <h2>Register</h2>
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
+    </AuthProvider>
   );
 };
 
