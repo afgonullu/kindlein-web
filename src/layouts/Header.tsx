c8915510ccd86c49b3f809461b2df93f54c956a0
A Faruk Gonullu <afgonullu@gmail.com>
2021-01-29 01:13:51 +0300

    build: header layout component

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,20 +1,34 @@
 import React from "react";
+import { Container, Navbar, Nav } from "react-bootstrap";
 import { NavLink } from "react-router-dom";
+import { Home, LogIn } from "react-feather";
+
+import logo from "../assets/images/logo.png";
 
 const Header: React.FC = () => {
   return (
-    <header>
-      <h1>Kindlein</h1>
-      <nav>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="/login">Login</NavLink>
-          </li>
-        </ul>
-      </nav>
+    <header className="kl-header">
+      <Container>
+        <Navbar expand="lg">
+          <Navbar.Brand className="brand-wrapper mb-0" href="/">
+            <img src={logo} alt="logo" className="logo" />
+            Kindlein
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <NavLink className="nav-link" to="/">
+                <Home />
+                <span>Home</span>
+              </NavLink>
+              <NavLink className="nav-link" to="/login">
+                <LogIn />
+                <span>Login</span>
+              </NavLink>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+      </Container>
     </header>
   );
 };
